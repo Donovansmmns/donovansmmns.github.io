7784b2696cb4b7833597cda893fab1a22a232f44
Simmons0601656 <0601656@sps-student.sk.ca>
2019-09-09 14:11:09 -0600

    Update sketch.js

--- lineart/sketch.js
+++ lineart/sketch.js
@@ -1,33 +1,19 @@
-// Project Title
-// Your Name
-// Date
+// Line Art Demo
+// Donovan Simmons
+// Sept 9, 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let redAmount = 0;
-let redChangeAmount = 1;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background(255);
 }
 
 function draw() {
-  fill(redAmount, 0, 255);
-  noStroke();
-  ellipse(mouseX, mouseY, 100, 100);
-
-  redAmount += redChangeAmount;
-
-  if (redAmount >= 255) {
-    redChangeAmount *= -1;
-  }
-  if (redAmount <= 0) {
-    redChangeAmount *= -1;
-  }
-
-  console.log(redAmount);
+  line(mouseX, mouseY, pmouseX, pmouseY);
   
 
 }
