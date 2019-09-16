7d0879e229767f51767368694181f196995cf907
Simmons0601656 <0601656@sps-student.sk.ca>
2019-09-16 14:01:24 -0600

    Made ball/square bounce around screen

--- imagedemo/sketch.js
+++ imagedemo/sketch.js
@@ -1,11 +1,32 @@
+let spongebob;
+let scalar =1;
+function preload() {
+  spongebob = loadImage("assets/spongebob.png");
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
   
 }
 
 function draw() {
-  ellipse(windowWidth / 2, windowHeight / 2)
+  background(255);
 
-
+  if (keyIsPressed){
+    if (keyCode === UP_ARROW){
+      console.log("Up arrow pressed.");
+      scalar *= 1.02;
+      
+    }
+    else if (keyCode === DOWN_ARROW) {
+      console.log("Down arrow pressed.");
+      scalar /= 1.02;
+  }
 }
+  imageMode(CENTER);
+  image(spongebob, mouseX, mouseY, spongebob.width * scalar, spongebob.height * scalar);
+  
+}
+
+
