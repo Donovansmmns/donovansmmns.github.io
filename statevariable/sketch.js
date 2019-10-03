7ba8f45beb3665fe10d903b472249f8082948fbb
Simmons0601656 <0601656@sps-student.sk.ca>
2019-10-03 14:15:24 -0600

    added platforms and fixed part of clicking bug

--- statevariable/sketch.js
+++ statevariable/sketch.js
@@ -5,6 +5,7 @@
 //Extra For Experts.
 let playerX;
 let playerY;
+let coinsCollected;
 
 let state = "start";
 let level;
@@ -17,18 +18,21 @@ function setup(){
 
 function draw(){
     background(255);
-    startMenu();
-    levelSelect();
+    if (state === "start"){
+        startMenu();
+    }
+    else if (state === "level"){
+        levelSelect();
+    }
     levelOne();
 }
 
 function startMenu(){
-    if (state === "start"){
+    
         textAlign(CENTER, CENTER);
         fill(0);
         textSize(50);
         text("Welcome to  \nTHE GAME.\n Enjoy it ☺", width/2, height/2)
-    }
 
     if (mouseIsPressed){
         state = "level";
@@ -45,14 +49,14 @@ function levelSelect(){
         textSize(80);
         text("LEVEL SELECT", width/2, height/11);
         text("01", width/4+55, height/5+15)
-
+       
         if (mouseX <width/4+100 && mouseX > width/4 && mouseY < height/5+100 && mouseY > height/5){
             if (mouseIsPressed){
                 if (state === "level"){
                     state = "level 1"
                 }
             }
-        }
+        }  
     }
 }
 
@@ -60,5 +64,9 @@ function levelOne(){
     if (state === "level 1"){
         fill(0);
         rect(0, height-50, width, 50);
+        rect(width - 100, height - 250, 100, 25)
+        rect(width - 400, height - 400, 50, 25);
+        rect(width/2-50, height/2 - 50, 100, 25)
     }
 }
+
