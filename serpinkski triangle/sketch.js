6d4d09c5c6a3ef5ee2623df3206a2a2ce3e42c2c
Donovansmmns <Donovansmmns@hotmail.com>
2019-12-04 14:12:20 -0600

    demo

--- serpinkski triangle/sketch.js
+++ serpinkski triangle/sketch.js
@@ -1,4 +1,4 @@
-//Sierpinkski Triangle DEMO
+// Sierpinski Triangle
 
 let triangleVertices = [
   {x: 400, y: 100},
@@ -7,30 +7,51 @@ let triangleVertices = [
 ];
 
 let depth = 0;
-
+let colorList = ["red", "blue", "green", "teal", "black", "white", "aquamarine", "maroon", "violet"]
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(255)
+  background(220);
   sierpinski(triangleVertices, depth);
 }
 
-function sierpinski(points, level){
-  triangle(points[0].x, points[0].y, 
-          points[1].x, points[1].y, 
-          points[2].x, points[2].y);
-
-  if (level > 0) {
-    sierpinski([points[0], getMidpoint(points[0], points[1]), 
-                          getMidpoint(points[0], points[2])], 
-                          level - 1)
+function mousePressed() {
+  if (depth < 10){
+    depth++;
   }
 }
 
+function sierpinski(points, level) {
+  noStroke();
+  if (level > colorList.length - 1){
+    colorList.push(color(random(255), random(255), random(255)))
+  }
+  fill(colorList[level]);
+  triangle(points[0].x, points[0].y, 
+           points[1].x, points[1].y,
+           points[2].x, points[2].y);
 
-function getMidpoint(point1, point2){
+  if (level > 0) {
+    sierpinski([points[0],
+                getMidpoint(points[0], points[1]),
+                getMidpoint(points[0], points[2])],
+                level - 1);
+    
+    sierpinski([points[1],
+                getMidpoint(points[0], points[1]),
+                getMidpoint(points[1], points[2])],
+                level - 1);
+
+    sierpinski([points[2],
+                getMidpoint(points[2], points[1]),
+                getMidpoint(points[0], points[2])],
+                level - 1);
+  }
+}
+
+function getMidpoint(point1, point2) {
   let midX = (point1.x + point2.x) / 2;
   let midY = (point1.y + point2.y) / 2;
   return {x: midX, y: midY};
