5fd25c4455844e26432974727b56ad3ff6b9e6e9
Donovansmmns <Donovansmmns@hotmail.com>
2019-12-06 14:08:15 -0600

    added demo for local storage IMPORTANT

---
+++ localStorageDemo/sketch.js
@@ -0,0 +1,34 @@
+// Local Storage Demo
+
+let numberOfClicks = 0;
+let allTimeHighestClicks;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  if (getItem("allTimeHighestClicks") !== null){
+    allTimeHighestClicks = getItem("highestClicks")
+  }
+  else{
+    allTimeHighestClicks = 0;
+  }
+}
+
+function draw() {
+  background(255);  
+
+  textSize(72)
+  fill(0);
+  text(numberOfClicks, 50, 100);
+
+  fill("green");
+  text(allTimeHighestClicks, 50, 200);
+}
+
+function mousePressed(){
+  numberOfClicks++;
+
+  if (numberOfClicks > allTimeHighestClicks){
+    allTimeHighestClicks = numberOfClicks;
+    storeItem("highestClicks", allTimeHighestClicks)
+  }
+}
